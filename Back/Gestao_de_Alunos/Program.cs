53fbaff3ee8ce4c1e8824787ca5aa0009be88b02
Rafael <rafavelloso2@gmail.com>
2026-03-22 23:45:37 +0000

    adiciona camada de dados e atualiza estrutura do projeto

--- Back/Gestao_de_Alunos/Program.cs
+++ Back/Gestao_de_Alunos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace Gestao_de_Alunos
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=DESKTOP-42RL6N1;Database=Gestao_de_Alunos;User Id=sa;Password=135113rr;";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    Console.WriteLine("Conexão bem-sucedida!");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Erro de SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro geral: " + ex.Message);
+            }
         }
     }
 }
