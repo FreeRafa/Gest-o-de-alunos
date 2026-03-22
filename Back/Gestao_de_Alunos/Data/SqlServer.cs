8a215cbf31240ad7caed847ea8a5a7e5a6e89ef6
Rafael <rafavelloso2@gmail.com>
2026-03-22 23:45:37 +0000

    adiciona camada de dados e atualiza estrutura do projeto

---
+++ Back/Gestao_de_Alunos/Data/SqlServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Alunos
+{
+    public class SqlServer
+    {
+        private static readonly string connectionString =
+          "Server=DESKTOP-42RL6N1;Database=Gestao_de_Alunos;User Id=sa;Password=135113rr;";
+
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+    }
+}
