5d65da84493cc49a5be158eb1f60448eab4e21da
Rafael <rafavelloso2@gmail.com>
2026-03-22 19:49:51 +0000

    Classes implementadas

---
+++ Back/Gestao_de_Alunos/Model/Matricula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Alunos.Model
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public StatusMatricula status { get; set; }
+
+        public Aluno aluno { get; set; }
+        public int Id_Aluno { get; set; }
+
+        public Disciplina Disciplina { get; set; }
+        public int Id_Disciplina { get; set; }
+    }
+
+    public enum StatusMatricula
+    {
+        Ativa = 1,
+        Trancada = 2,
+        Concluida = 3
+    }
+}
---
+++ Back/Gestao_de_Alunos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Alunos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
