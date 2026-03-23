f098220ca7f1311356c8b1e3d3dc14201d131198
Rafael <rafavelloso2@gmail.com>
2026-03-23 23:31:52 +0000

    feat: atualiza repositório e serviço de Aluno

--- Back/Gestao_de_Alunos/Repositorio/RepositorioAluno.cs
+++ Back/Gestao_de_Alunos/Repositorio/RepositorioAluno.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gestao_de_Alunos.Model;
+
 
 namespace Gestao_de_Alunos.Repositorio
 {
@@ -13,6 +18,69 @@ namespace Gestao_de_Alunos.Repositorio
         public RepositorioAluno(string connectionString)
         {
             _ConnectionString = connectionString;
+        }
+
+        public void CriarAluno(Aluno aluno)
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString)) 
+            {
+                string query = @"INSERT INTO Aluno(Id, Nome, UltimoNOme, DataNascimento, Fone, Email) VALUES (@Id, @Nome, @UltimoNome, @DataNascimento, @Fone, @Email)";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                cmd.Parameters.Add("@Id", SqlDbType.Int).Value = aluno.Id;
+                cmd.Parameters.Add("@Nome", SqlDbType.NVarChar, 100).Value = aluno.Nome;
+                cmd.Parameters.Add("@UltimoNome", SqlDbType.NVarChar, 100).Value = aluno.UltimoNome;
+                cmd.Parameters.Add("@DataNascimento", SqlDbType.Date).Value = aluno.DataNascimento;
+                cmd.Parameters.Add("@Fone", SqlDbType.NChar, 50).Value = aluno.Fone;
+                cmd.Parameters.Add("@Email", SqlDbType.NChar, 100).Value = aluno.Email;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Aluno LerAluno(int Id) 
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString)) 
+            {
+                string query = "SELECT * FROM Aluno WHERE Id = @Id";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+
+                connection.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader()) 
+                {
+                    if (reader.Read())
+                    {
+                        Aluno aluno = new Aluno
+                        {
+                            Id = (int)reader["Id"],
+                            Nome = reader["Nome"].ToString(),
+                            UltimoNome = reader["UltimoNome"].ToString(),
+                            DataNascimento = Convert.ToDateTime(reader["DataNascimento"]),
+                            Fone = reader["Fone"].ToString(),
+                            Email = reader["email"].ToString(),
+                        };
+                        return aluno;
+                    }
+                    else 
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public void AtualizarAluno(Aluno aluno) 
+        {
+        }
+
+        public void DeletarAluno(int Id) 
+        {
         }
     }
 }
--- Back/Gestao_de_Alunos/Servico/ServicoAluno.cs
+++ Back/Gestao_de_Alunos/Servico/ServicoAluno.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gestao_de_Alunos.Model;
+using Gestao_de_Alunos.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Gestao_de_Alunos.Servico
 {
-    internal class ServicoAluno
+    public class ServicoAluno
     {
+        private readonly RepositorioAluno _alunoRepositorio;
+
+        public ServicoAluno(RepositorioAluno repositorioAluno) 
+        {
+            _alunoRepositorio = repositorioAluno;
+        }
+
+        public void CriarAluno(Aluno aluno) 
+        {
+            if (string.IsNullOrEmpty(aluno.Nome))
+                throw new Exception("O nome é obtogatório");
+
+            if (string.IsNullOrEmpty(aluno.UltimoNome))
+                throw new Exception("O ultimo nome é obtogatório");
+
+            //Verificar DATANASCIMENTO
+
+            _alunoRepositorio.CriarAluno(aluno);
+
+        }
+
+        public Aluno LerAluno(int Id) 
+        {
+           return _alunoRepositorio.LerAluno(Id);
+
+        }
+
+        public void AtualizarAluno(Aluno aluno) 
+        {
+        }
+
+        public void DeletarAluno(int Id) 
+        {
+        }
     }
 }
