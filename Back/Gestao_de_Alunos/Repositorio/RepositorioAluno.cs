59784cd4b33c6aba10fbc13f07183b79eb9b6d9e
Rafael <rafavelloso2@gmail.com>
2026-03-23 00:18:35 +0000

    adiciona repositorio de aluno e atualiza projeto

---
+++ Back/Gestao_de_Alunos/Repositorio/RepositorioAluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Alunos.Repositorio
+{
+    public class RepositorioAluno
+    {
+        private readonly string _ConnectionString;
+
+        public RepositorioAluno(string connectionString)
+        {
+            _ConnectionString = connectionString;
+        }
+    }
+}
