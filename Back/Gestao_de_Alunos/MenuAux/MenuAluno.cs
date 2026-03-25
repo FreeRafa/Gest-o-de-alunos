71bbfcc1e48902ed208e8df099131b055541f634
Rafael <rafavelloso2@gmail.com>
2026-03-25 23:57:34 +0000

    Implementa camada de repositorio e servico para entidade Professor

---
+++ Back/Gestao_de_Alunos/MenuAux/MenuAluno.cs
@@ -0,0 +1,62 @@
+﻿using Gestao_de_Alunos.Model;
+using Gestao_de_Alunos.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Alunos.MenuAux
+{
+    public class MenuAluno
+    {
+        private ServicoAluno servicoAluno;
+
+        public MenuAluno(ServicoAluno _servicoAluno)
+        {
+            servicoAluno = _servicoAluno;
+        }
+
+        public void MatriculaAluno() 
+        {
+            int opcao = -1;
+
+            while (opcao != 0)
+            {
+                Console.WriteLine("=== Menu de Aluno ===");
+                Console.WriteLine("1 .Login");
+                Console.WriteLine("2 .Cadastrar");
+              
+                Console.Write("Escolha uma opção: ");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+
+
+                        break;
+
+                    case 2:
+
+                        Aluno aluno = new Aluno();
+
+                        Console.WriteLine("Cadastro:");
+
+                        
+
+                        Console.WriteLine("Aluno cadastrado com sucesso!");
+
+                        break;
+
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Back/Gestao_de_Alunos/Repositorio/RepositorioProfessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gestao_de_Alunos.Model;
+
+namespace Gestao_de_Alunos.Repositorio
+{
+    public class RepositorioProfessor
+    {
+        private readonly string _ConnectionString;
+
+        public RepositorioProfessor(string connectionString) 
+        {
+            _ConnectionString = connectionString;
+        }
+
+        public void CriarProfessor(Professor professor) 
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                string query = @"INSERT INTO Professor(Nome, UltimoNOme) VALUES (@Nome, @UltimoNome)";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                cmd.Parameters.Add("@Nome", SqlDbType.NVarChar, 100).Value = professor.Nome;
+                cmd.Parameters.Add("@UltimoNome", SqlDbType.NVarChar, 100).Value = professor.UltimoNome;
+                
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
